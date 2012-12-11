8e41e242b2d59d4c6474b6b78cae8b38f0cfa064
Rui Araújo <ruka.araujo@gmail.com>
2012-12-11 17:35:49 +0100

    Bump the version to 3.0.1

--- android/src/org/exobel/routerkeygen/ui/Preferences.java
+++ android/src/org/exobel/routerkeygen/ui/Preferences.java
@@ -93,8 +93,8 @@ public class Preferences extends SherlockPreferenceActivity {
 	private static final String PUB_DIC_CFV = "http://android-thomson-key-solver.googlecode.com/svn/trunk/RKDictionary.cfv";
 	private static final String PUB_VERSION = "http://android-thomson-key-solver.googlecode.com/svn/trunk/RouterKeygenVersion.txt";
 
-	private static final String VERSION = "3.0.0";
-	private static final String LAUNCH_DATE = "04/01/2012";
+	private static final String VERSION = "3.0.1";
+	private static final String LAUNCH_DATE = "11/12/2012";
 
 	private String version = "";
 
