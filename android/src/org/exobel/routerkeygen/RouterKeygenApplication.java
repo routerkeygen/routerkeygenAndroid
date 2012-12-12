f7ffd63707ff2736e43eeae34506591e0db58d3a
Rui Araújo <ruka.araujo@gmail.com>
2012-12-12 22:00:39 +0100

    Bump version to v3.0.2

--- android/src/org/exobel/routerkeygen/RouterKeygenApplication.java
+++ android/src/org/exobel/routerkeygen/RouterKeygenApplication.java
@@ -5,7 +5,7 @@ import org.acra.annotation.ReportsCrashes;
 
 import android.app.Application;
 
-@ReportsCrashes(formKey = "dE5pQldqS3Vycjh6Ui1zejlLOHd3dlE6MQ") 
+@ReportsCrashes(formKey = "dHhJWWdhVDZOTnAwaWZMTngxY1pYY0E6MQ") 
 public class RouterKeygenApplication extends Application {
   @Override
   public void onCreate() {
