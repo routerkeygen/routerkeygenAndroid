8c793542bc9be4c0b44eb4d90119b0d7e2e07a77
Rui Araújo <ruka.araujo@gmail.com>
2012-12-30 17:17:13 +0000

    Workaround a bug in some devices.

--- android/src/org/exobel/routerkeygen/RouterKeygenApplication.java
+++ android/src/org/exobel/routerkeygen/RouterKeygenApplication.java
@@ -5,11 +5,15 @@ import org.acra.annotation.ReportsCrashes;
 
 import android.app.Application;
 
-@ReportsCrashes(formKey = "dDJGUGp2VUE1cFhDaDZzVUZjb2dSdUE6MQ") 
+@ReportsCrashes(formKey = "dDJGUGp2VUE1cFhDaDZzVUZjb2dSdUE6MQ")
 public class RouterKeygenApplication extends Application {
-  @Override
-  public void onCreate() {
-    ACRA.init(this);
-    super.onCreate();
-  }
-}	
+	@Override
+	public void onCreate() {
+		try {
+			ACRA.init(this);
+		} catch (Exception e) {
+
+		}
+		super.onCreate();
+	}
+}
