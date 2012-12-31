ca5ba7286a1155abc2f9cbf2f0c230c9aecc8e58
Rui Araújo <ruka.araujo@gmail.com>
2012-12-31 15:31:50 +0000

    Updated Analytics code and crash report.

--- android/src/org/exobel/routerkeygen/RouterKeygenApplication.java
+++ android/src/org/exobel/routerkeygen/RouterKeygenApplication.java
@@ -5,7 +5,7 @@ import org.acra.annotation.ReportsCrashes;
 
 import android.app.Application;
 
-@ReportsCrashes(formKey = "dC1UWDZYbHYtWUVRSXUzWlh3a0V0WlE6MQ")
+@ReportsCrashes(formKey = "dHYtYXNPZ1NRajdPLUZyaFc0OTd5cXc6MQ")
 public class RouterKeygenApplication extends Application {
 	@Override
 	public void onCreate() {
