a4c6d1df469f7d44f34208e32b589fb042113c6f
Rui Araújo <ruka.araujo@gmail.com>
2013-02-05 00:39:52 +0100

    Moved to email based crash report.

--- android/src/org/exobel/routerkeygen/RouterKeygenApplication.java
+++ android/src/org/exobel/routerkeygen/RouterKeygenApplication.java
@@ -1,11 +1,16 @@
 package org.exobel.routerkeygen;
 
 import org.acra.ACRA;
+import org.acra.ReportField;
+import org.acra.ReportingInteractionMode;
 import org.acra.annotation.ReportsCrashes;
 
 import android.app.Application;
 
-@ReportsCrashes(formKey = "dDhYOTFNQlJnLTBJR200Ylhua0wyUkE6MQ")
+@ReportsCrashes(formKey = "", mailTo = "exobel@gmail.com", customReportContent = {
+		ReportField.APP_VERSION_NAME, ReportField.ANDROID_VERSION,
+		ReportField.PHONE_MODEL, ReportField.CUSTOM_DATA,
+		ReportField.STACK_TRACE, ReportField.LOGCAT }, mode = ReportingInteractionMode.TOAST, resToastText = R.string.crash_toast_text)
 public class RouterKeygenApplication extends Application {
 	@Override
 	public void onCreate() {
